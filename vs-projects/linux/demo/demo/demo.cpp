3c7b3e56cecd1936d939bc63ed52c105e0340240
Palanisamy <Gowthaman.Palanisamy@FISGLOBAL.COM>
2022-04-08 13:35:25 +0530

    vs-projects added

---
+++ vs-projects/linux/demo/demo/demo.cpp
@@ -0,0 +1,12 @@
+﻿// demo.cpp : Defines the entry point for the application.
+//
+
+#include "demo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ vs-projects/linux/hello-demo/hello-demo/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from %s!\n", "hello_demo");
+    return 0;
+}
